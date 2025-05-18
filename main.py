483af2387ab1873bcf50e697504085622534cfec
nguyen-hau204 <nguyenhautq2k4@gmail.com>
2025-05-18 18:52:52 +0700

    update tls mogo

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 # ======== MongoDB ========
-mongo_client = MongoClient("mongodb+srv://nguyenhautq2k4:aSIAzthfbxdSjI9k@dbmess.quy0clt.mongodb.net/?retryWrites=true&w=majority&appName=dbMess")
+mongo_client = MongoClient("mongodb+srv://nguyenhautq2k4:aSIAzthfbxdSjI9k@dbmess.quy0clt.mongodb.net/?retryWrites=true&w=majority&tls=true")
+
+
 db = mongo_client["qa_database"]
 qa_collection = db["custom_qa"]
 
